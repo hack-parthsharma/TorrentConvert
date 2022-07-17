3ad42872a268329a94e6199b541e07c1420d7e01
Parth Sharma <69046031+hack-parthsharma@users.noreply.github.com>
2022-07-17 14:04:17 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# Torrent Converter
+( Live: [@TorrentToMagnetRobot](https://t.me/TorrentToMagnetRobot) )
+
+A Telegram bot to convert Torrent files -> magnet links & vice versa.
+
+Works in Groups & private conversations.
+
+# Usage
+1. `git clone https://github.com/hack-parthsharma/TorrentConvert`
+2. `cd TorrentConvert`
+3. `cp .env.example .env`
+4. Fill in `.env`
+5. `npm install`
+6. `npm start`
+
+## HEROKU is NOT supported:
+
+<details>
+  <summary>Proof</summary>
+  <img src="https://user-images.githubusercontent.com/48287817/138405287-7aa7002e-83f0-46f4-bbcf-eb87ea72cd8c.png" width="400">
+</details>
+
+## Credits
+- Developers of [m2t](https://www.npmjs.com/package/magnet2torrent-js), [t2m](https://www.npmjs.com/package/torrent2magnet) 😍
+- [grammy](https://grammy.dev/) Framework 💙
+- VPS 😌
---
+++ bot.js
@@ -0,0 +1,65 @@
+// A simple magnet->torrent & torrent->magnet converter Telegram bot.
+// ToDo: DB, itorrents, Logger, Groups /cmd@bot magnetlink support.
+
+require('dotenv').config();
+const { startMsg, helpMsg, aboutMsg } = require('./helpers/commands');
+const { getTorrentFile, getMagnetLink } = require('./helpers/utils');
+
+const { Bot, InputFile, GrammyError, HttpError } = require("grammy");
+const { hydrateFiles } = require("@grammyjs/files");
+const bot = new Bot(process.env.BOT_TOKEN);
+bot.api.config.use(hydrateFiles(bot.token));
+
+// Commands
+bot.command("start", (ctx) => {
+    startMsg(ctx);
+});
+
+bot.command("help", (ctx) => {
+    helpMsg(ctx);
+});
+
+bot.command("about", (ctx) => {
+    aboutMsg(ctx);
+});
+
+// Handle text messages
+bot.on("message:text", async (ctx) => {
+    const mesg = ctx.message.text;
+    var words = mesg.split(' ');
+    if(mesg.startsWith('magnet:?')){
+        const magnet = mesg;
+        getTorrentFile(ctx, magnet, InputFile);
+    }
+    else if ( mesg.startsWith("/") && words[1]?.startsWith('magnet:?') ) {
+        const magnet = words[1];
+        getTorrentFile(ctx, magnet, InputFile);
+    }
+});
+
+// Handle files
+bot.on('message:document').filter(ctx => ctx.message.document.mime_type === 'application/x-bittorrent', async (ctx) => {
+    var status = await ctx.reply("✍️",
+    {
+        parse_mode: "HTML",
+        reply_to_message_id: ctx.message.message_id
+    });
+    getMagnetLink(ctx, status);
+    
+})
+
+bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+      console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+      console.error("Could not contact Telegram:", e);
+    } else {
+      console.error("Unknown error:", e);
+    }
+  });
+
+// Start bot
+bot.start();
